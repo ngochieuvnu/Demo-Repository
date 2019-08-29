d831570b6bfd8e814b592b9a413cadc0ea14d10d
unknown <ngochieuvnu@gmail.com>
2019-08-30 01:44:14 +0700

    Commit from hieuvn1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static bool isPrime(int number)
+        {
+            if (number < 2) return false;
+            else
+            {
+                int i = 2;
+                while (i <= number / 2)
+                {
+                    if (number % i == 0) return false;
+                    i++;
+                }
+                return true;
+            }
+
+        }
+            
+        static void Main(string[] args)
+        {
+            int number = 11;
+            Console.WriteLine(isPrime(number));
+            Console.ReadKey();
+        }
+    }
+}
